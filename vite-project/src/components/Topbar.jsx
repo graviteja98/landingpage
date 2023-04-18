dd265bec2603b33e709fe7bd4a3c1da5d1a3edeb
graviteja98 <gaddameediraviteja98@gmail.com>
2023-04-18 12:45:08 +0530

    fixed the topbar

--- vite-project/src/components/Topbar.jsx
+++ vite-project/src/components/Topbar.jsx
@@ -4,14 +4,12 @@ import React from 'react'
 function Topbar() {
   return (
     <div>
-      <Container color="primary">
-        <Stack direction="row">
+      <Container color="primary" sx={{padding:"20px",background:"#ddd",display:"flex",justifyContent:"space-around"}}>
             <a>Home</a>
             <a>Products</a>
             <input type="text" placeholder='enter text'/>
             <a>Search</a>
             <a>Logout</a>
-        </Stack>
         </Container>
     </div>
   )
