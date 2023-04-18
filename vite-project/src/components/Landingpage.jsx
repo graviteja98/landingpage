56b8ec5a8d3535c2c31590bba0ca6fada292be54
Eashwar Reddy <eashwar950@gmail.com>
2023-04-18 11:13:48 +0530

    stack to column

--- vite-project/src/components/Landingpage.jsx
+++ vite-project/src/components/Landingpage.jsx
@@ -6,7 +6,7 @@ function Landingpage() {
     <div>
       <Stack direction="column">
       <Typography variant="h1">Landing page</Typography>
-      <Stack direction="row">
+      <Stack direction="column">
       <Button variant='contained'>Click Me</Button>
       <Button sx={{p:2}} variant='contained' color="secondary">Exit</Button>
       </Stack>
