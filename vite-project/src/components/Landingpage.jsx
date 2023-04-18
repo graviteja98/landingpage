0f75bce894e766baff1ea51253203ec71f991b21
graviteja98 <gaddameediraviteja98@gmail.com>
2023-04-18 12:28:16 +0530

    added top bar

--- vite-project/src/components/Landingpage.jsx
+++ vite-project/src/components/Landingpage.jsx
@@ -1,16 +1,16 @@
 import React from 'react'
 import { Typography,Button, Stack } from '@mui/material'
+import Topbar from './Topbar';
 
 function Landingpage() {
   return (
     <div>
+      <Topbar/>
       <Stack direction="column">
       <Typography variant="h1">Landing page</Typography>
-      <Stack direction="column">
+      <Stack direction="row">
       <Button variant='contained'>Click Me</Button>
       <Button sx={{p:2}} variant='outlined'>Exit</Button>
-      <Button variant='contained'>Log out</Button>
-      <Button sx={{p:2}} variant='outlined'>Cart</Button>
       </Stack>
       </Stack>
       </div>
